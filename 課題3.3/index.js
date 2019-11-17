0216cefd48dcf1d0ed898c72c2dafb4dc40ce522
a4e1 <asfor3215@gmail.com>
2019-11-17 11:25:12 +0900

    課題3.3追加

---
+++ 課題3.3/index.js
@@ -0,0 +1,20 @@
+let num = 3
+
+if(num % 2 == 0){
+    alert("2の倍数です")
+}else {
+    alert("2の倍数ではないです")
+}
+
+let num = 3
+
+switch(num){
+    case 2:
+        alert("2")
+        break
+    case 3:
+        alert("3")
+        break
+    default:
+        alert("2でも3でもありません")
+}
