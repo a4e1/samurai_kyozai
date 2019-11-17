2da002742f6286a32aca99d3e9554edb6545cc3e
a4e1 <asfor3215@gmail.com>
2019-11-17 18:24:17 +0900

    5章DOM課題前

---
+++ 課題5.3/index.js
@@ -0,0 +1,10 @@
+let insert = () => {
+    let str = prompt("何か入力してください！")
+
+    let ul = document.getElementById("dom")
+
+    let li = document.createElement('li')
+    li.innerText = str
+
+    ul.appendChild(li)
+}
